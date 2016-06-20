ecbe8605a665dff87cabdc4a6d80e99a78180869
Mehmet Ataş <mehmetatasm@gmail.com>
2016-06-20 21:09:48 +0300

    main folders renamed

--- auth/src/main/java/net/mehmetatas/apps/auth/controllers/messages/SignupRequest.java
+++
@@ -1,9 +0,0 @@
-package net.mehmetatas.apps.auth.controllers.messages;
-
-/**
- * Created by mehmet on 03.06.2016.
- */
-public class SignupRequest {
-    public String email;
-    public String password;
-}
--- webui/readme.md
+++
@@ -1,1 +0,0 @@
-http://mehmetatas.net/apps
