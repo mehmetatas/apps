8bd77dfec16a1fc2a760656bf27d1082bd971cd9
Mehmet Ataş <mehmetatasm@gmail.com>
2016-06-21 21:12:04 +0300

    signup connected to server

--- services/auth/src/main/java/net/mehmetatas/apps/auth/AuthApplication.java
+++ services/auth/src/main/java/net/mehmetatas/apps/auth/AuthApplication.java
@@ -3,10 +3,12 @@ package net.mehmetatas.apps.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AuthApplication {
-
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(AuthApplication.class, args);
 	}
 }
--- ui/src/app/auth/signup.controller.js
+++ ui/src/app/auth/signup.controller.js
@@ -6,8 +6,13 @@
 		.controller("SignUpController", SignUpController);
 
     /* @ngInject */
-    function SignUpController() {
+    function SignUpController($http) {
         var vm = this;
+        
+        vm.email = "";
+        vm.password = "";
+        vm.signup = signup;
+        vm.username = "";
 
         // activate
         activate();
@@ -17,5 +22,24 @@
         }
 
         // vm members
+        function signup() {
+            $http({
+                method: "POST",
+                url: "http://localhost:8080/api/v1/auth/signup",
+                data: {
+                    username: vm.username,
+                    email: vm.email,
+                    password: vm.password
+                },
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                }
+            }).then(function successCallback(response) {
+                alert("ok");
+            }, function errorCallback(response) {
+                alert(response.data.message);
+            });
+        }
     }
 })();
